d45cb5658ad2b00883d2a0eaeb17fb1f835ce531
Hastings Greer <hastings.greer@gmail.com>
2022-04-06 12:29:35 -0400

    pip install -e + type hint fix

--- src/itk_torch_transform_bridge.py
+++ src/itk_torch_transform_bridge.py
@@ -1,7 +1,9 @@
 import itk
 import torch
 
-def itk_transform_bridge(image_fixed: "itk.Image", image_moving:"itk.Image", network_shape: "[int]", **kwargs)->"(torch.Tensor, torch.Tensor, Callable[torch.Tensor, itk.Transform])":
+
+
+def itk_transform_bridge(image_fixed: "itk.Image", image_moving:"itk.Image", network_shape: "[int]", **kwargs)->"(torch.Tensor, torch.Tensor, Callable[[torch.Tensor], itk.Transform])":
     # Convert images to tensors
 
     # ...
