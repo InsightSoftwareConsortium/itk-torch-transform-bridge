fadfb28efc0fcd5c52b59184c27bee525664f9b8
HastingsGreer <hastings.greer@gmail.com>
2022-04-06 14:48:06 -0400

    Update itk_torch_transform_bridge.py

--- src/itk_torch_transform_bridge.py
+++ src/itk_torch_transform_bridge.py
@@ -26,4 +26,41 @@ def itk_transform_bridge(image_fixed: "itk.Image", image_moving:"itk.Image", net
 
     return tensor_fixed, tensor_moving, convert_back
 
-
+def resampling_transform(image, shape) -> itk.Transform:
+    
+    imageType = itk.template(image)[0][itk.template(image)[1]]
+    
+    dummy_image = itk.image_from_array(np.zeros(tuple(reversed(shape)), dtype=itk.array_from_image(image).dtype))
+    if len(shape) == 2:
+        transformType = itk.MatrixOffsetTransformBase[itk.D, 2, 2]
+    else:
+        transformType = itk.VersorRigid3DTransform[itk.D]
+    initType = itk.CenteredTransformInitializer[transformType, imageType, imageType]
+    initializer = initType.New()
+    initializer.SetFixedImage(dummy_image)
+    initializer.SetMovingImage(image)
+    transform = transformType.New()
+    
+    initializer.SetTransform(transform)
+    initializer.InitializeTransform()
+    
+    if len(shape) == 3:
+        transformType = itk.CenteredAffineTransform[itk.D, 3]
+        t2 = transformType.New()
+        t2.SetCenter(transform.GetCenter())
+        t2.SetOffset(transform.GetOffset())
+        transform = t2
+    m = transform.GetMatrix()
+    m_a = itk.array_from_matrix(m)
+    
+    input_shape = image.GetLargestPossibleRegion().GetSize()
+    
+    for i in range(len(shape)):
+    
+        m_a[i, i] = image.GetSpacing()[i] * (input_shape[i] / shape[i])
+    
+    m_a = itk.array_from_matrix(image.GetDirection()) @ m_a 
+    
+    transform.SetMatrix(itk.matrix_from_array(m_a))
+    
+    return transform
